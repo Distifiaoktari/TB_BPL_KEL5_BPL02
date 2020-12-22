a4e336adb59d26b85342b9f228e4dac2958e58fa
Gnd10 <60834447+Gnd10@users.noreply.github.com>
2020-12-22 11:22:17 +0700

    Create datatransaksi.java

---
+++ src/tb_bpl_kel5_bpl02/datatransaksi.java
@@ -0,0 +1,27 @@
+package tb_bpl_kel5_bpl02;
+public class datatransaksi {
+public void getAll() {
+		
+	}
+	public void skuinput() {
+		
+	}
+	public void inputprocess() {
+		
+	}
+	public void Jumlahbarang() {
+		
+	}
+	public void jmlprocess() {
+		
+	}
+	public void hasilorder() {
+		
+	}
+	public void input() {
+		
+	}
+	public void show() {
+		
+	}
+}
